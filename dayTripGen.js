57c73d970075d66627b32fd785714af1220f3531
Dom B <domlamont@yahoo.com>
2021-10-29 11:27:41 -0700

    Added the Day Trip Generator Blueprint to the JavaScript file

---
+++ dayTripGen.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//1: Destination (Array)
+//2: Restaurant (Array)
+//3: Mode of Transportation (Array)
+//4: Entertainment (Array)
+//5: I want to be able to randomly re-select a destination, 
+//restaurant, mode of transportation, and/or form of 
+//entertainment if I don’t like one or more of those things (Func)
+//6: Be able to Confirm Options (Func)
+//7: Display Trip to Console or Alert (Func)
+
+//Trip Options Array.
+let desArray = [];
+let foodArray = [];
+let transArray = [];
+let funArray = [];
+
+//Holds Selects Details.
+let selectedDes = [];
+let selectedFood = [];
+let selectedTrans = [];
+let selectedFun = [];
+
+function randomSelect(){
+
+}
+
+function reSelect(){
+
+}
+
+function confirm(){
+
+}
+
+function showTrip(){
+
+}
